1de8595c9916be371be547e985f577a25370a87e
mhmmd <mhmmd.rezaei4@gmail.com>
2023-03-01 07:21:16 +0330

    Function: setItem

--- enchantments.js
+++ enchantments.js
@@ -5,3 +5,15 @@ function getItem(cards, position) {
 }
 
 // console.log(getItem([1, 2, 4, 1], position));
+
+///--------///
+
+// const position = 2;
+// const replacementCard = 6;
+
+function setItem(cards, position, replacementCard) {
+  cards[position] = replacementCard;
+  return cards;
+}
+
+// console.log(setItem([1, 2, 4, 1], position, replacementCard));
